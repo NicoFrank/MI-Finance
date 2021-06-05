20dfb981d24d1bcde5d056c8d5bbbd3254182a5e
NicoFrank <nicolas.frank@st.oth-regensburg.de>
2021-06-05 17:54:00 +0200

    Tests succesful, website working. Needs more testing and maybe template

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+//python3 -m check50 --local mheckner/mi-check50/master/finance
+
+
+
 const express = require("express");
 const dotenv = require("dotenv");
 const helpers = require("./helpers");
 var bodyParser = require("body-parser");
 var pg = require("pg");
 var session = require("express-session");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 
 /* Reading global variables from config file */
@@ -11,7 +17,7 @@ dotenv.config();
 const PORT = process.env.PORT;
 
 const conString = process.env.DB_CON_STRING;
-if(conString == undefined){
+if (conString == undefined) {
   console.log("ERROR: enviroment variable DB_CON_STRING not set.");
   process.exit(1);
 }
@@ -25,16 +31,18 @@ const dbConfig = {
 
 //pg.default.ssl = true;
 
-var dbClient  = new pg.Client(dbConfig);
+var dbClient = new pg.Client(dbConfig);
 dbClient.connect();
 
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var urlencodedParser = bodyParser.urlencoded({
+  extended: false
+});
 /*
  *
  * Express setup
  *
-*/
+ */
 
 app = express();
 
@@ -45,118 +53,319 @@ app.set("views", "views");
 app.set("view engine", "pug");
 
 app.use(session({
-    secret: "This is a secret!",
-    resave: true,
-    saveUninitialized: true
+  secret: "This is a secret!",
+  cookie: {
+    maxAge: 600000
+  }, //(10min)
+  resave: true,
+  saveUninitialized: true
 }));
 
-
-/*app.get("/", async(req, res) => {
-    try {
-        let result = await helpers.lookup("NFLX");
-        res.send(result.body);
-
-    } catch(err) {
-        console.log(helpers.API_KEY);
-        console.log(err);
-        res.status(400).send("Sorry, Fehler beim Abrufen des Aktienkurses.");
-    }
-});*/
-
-app.get("/", function(req, res){
-    res.render("login");
+function initSession(session) {
+  if (session.user == undefined) {
+    session.user = ""
+    session.userid = -1
+    session.loogedin = false
+  } else {
+    return false
+  }
+}
+app.get("/quote", function(req, res) {
+  if (req.session.loogedin) res.render("quote");
+  else {
+    res.render("login", {
+      error: "You need to be logged in to access this page."
+    });
+  }
 });
-app.get("/login",function(req, res){
+app.post("/quote", urlencodedParser, async (req, res) => {
+  let symbol = req.body.symbol;
+  try {
+    let result = await helpers.lookup(symbol);
+
+    res.render("quote", {
+      result: result.latestPrice,
+      name: result.companyName,
+      symbol: symbol
+    });
+
+  } catch (err) {
+    res.status(400).render("quote", {
+      error: "ungültiges Tickersymbol"
+    });
+  }
+});
+
+app.get("/buy", function(req, res) {
+  if (req.session.loogedin) res.render("buy");
+  else {
+    res.render("login", {
+      error: "You need to be logged in to access this page."
+    });
+  }
+});
+
+app.post("/buy", urlencodedParser, async (req, res) => {
+  let symbol = req.body.symbol;
+  let count = Number(req.body.shares);
+  let user_id = req.session.userid;
+
+  try {
+    let result = await helpers.lookup(symbol);
+    dbClient.query("SELECT * FROM finance_overview WHERE user_id=$1 AND symbol='CASH'", [req.session.userid], function(dbError, dbResponse) {
+      if (dbResponse.rows.length != 0) {
+        let cash = dbResponse.rows[0].total;
+        let latest_price = Number(result.latestPrice);
+        let cost = Math.round(latest_price * count*100)/100;
+        let new_cash = cash - cost;
+
+        if (cost > cash) {
+          res.status(400).render("buy", {
+            error: "Nicht genug Geld"
+          });
+        } else if (count <= 0 || Number.isInteger(count) == false) {
+          res.status(400).render("buy", {
+            error: "ungültige Anzahl"
+          });
+        } else {
+          dbClient.query("SELECT * FROM finance_overview WHERE user_id=$1 AND symbol=$2", [user_id, symbol], function(dbError, dbResponse) {
+            if (dbResponse.rows.length != 0) {
+              let new_count = dbResponse.rows[0].count + count;
+              let new_total = cost + Number(dbResponse.rows[0].total); //dbResponse.row[0].total is a string?!
+              dbClient.query("UPDATE finance_overview SET count=$1, price=$2, total=$3 WHERE user_id=$4 AND symbol=$5", [new_count, latest_price, new_total, user_id, symbol]);
+            } else {
+              dbClient.query("INSERT INTO finance_overview (user_id, symbol, name, count, price, total) VALUES ($1, $2, $3, $4, $5, $6)", [user_id, symbol, result.companyName, count, latest_price, cost]);
+        }
+            dbClient.query("INSERT INTO finance_transactions (user_id, symbol, name, count, price, created_at) VALUES ($1, $2, $3, $4, $5, $6)", [user_id, symbol, result.companyName, count, latest_price, (new Date()).toLocaleString("en-US")]);
+            dbClient.query("UPDATE finance_overview SET total =$1 WHERE user_id=$2 AND symbol=$3", [new_cash, user_id, 'CASH']);
+            res.render("buy", {
+              success: "Kauf erfolgreich"
+            });
+          });
+        }
+      }
+    });
+  } catch (err) {
+    console.log(helpers.API_KEY);
+    console.log(err);
+    res.status(400).send("Sorry, Fehler beim Abrufen des Aktienkurses.");
+  }
+});
+
+app.post("/sell", urlencodedParser, async (req, res) => {
+  let symbol = req.body.symbol;
+  let count = Number(req.body.shares);
+  let user_id = req.session.userid;
+  let tickerItems =[];
+
+
+  dbClient.query("SELECT * FROM finance_overview WHERE user_id=$1 AND count>0",[req.session.userid], function(dbError, dbResponse){
+    tickerItems = dbResponse.rows;
+      });
+
+  try {
+    let result = await helpers.lookup(symbol);
+
+    dbClient.query("SELECT * FROM finance_overview WHERE user_id=$1 AND symbol='CASH'", [req.session.userid], function(dbError, dbResponse) {
+      if (dbResponse.rows.length != 0) {
+        let cash = Number(dbResponse.rows[0].total);
+        let latest_price = Math.round(Number(result.latestPrice)*100)/100;
+        let cost = latest_price * count;
+        let new_cash = cash + cost;
+
+        if (count <= 0 || Number.isInteger(count) == false) {
+          res.status(400).render("sell", {
+            error: "ungültige Anzahl",
+            tickerItems: tickerItems
+          });
+        } else {
+          dbClient.query("SELECT * FROM finance_overview WHERE user_id=$1 AND symbol=$2", [user_id, symbol], function(dbError, dbResponse) {
+            if (dbResponse.rows.length != 0) {
+              if (count > dbResponse.rows[0].count) {
+                res.status(400).render("sell", {
+                  error: "nicht genügend Aktien vorhanden",
+                  tickerItems: tickerItems
+                });
+              } else {
+                let new_count = dbResponse.rows[0].count - count;
+                let new_total = Math.round((Number(dbResponse.rows[0].total) - cost)*100)/100;
+                if (new_count == 0) {
+                  dbClient.query("DELETE FROM finance_overview WHERE user_id=$1 AND symbol= $2", [user_id, symbol]);
+                } else {
+                  dbClient.query("UPDATE finance_overview SET count=$1, price=$2, total=$3 WHERE user_id=$4 AND symbol=$5", [new_count, latest_price,new_total, user_id, symbol]);
+                }
+                dbClient.query("INSERT INTO finance_transactions (user_id, symbol, name, count, price, created_at) VALUES ($1, $2, $3, $4, $5, $6)", [user_id, symbol, result.companyName, -count, latest_price, new Date().toISOString().slice(0, 19).replace('T', ' ')]);
+                dbClient.query("UPDATE finance_overview SET total=$1 WHERE user_id=$2 AND symbol=$3", [Math.round(new_cash * 100) / 100, user_id, 'CASH']);
+
+                res.render("sell", {
+                  success: "verkauf erfolgreich",
+                  tickerItems: tickerItems
+                });
+              }
+            } else {
+              res.status(400).render("sell", {
+                error: "Aktie nicht im Depot",
+                tickerItems: tickerItems
+              });
+            }
+          });
+        }
+      }
+    });
+  } catch (err) {
+    console.log(helpers.API_KEY);
+    console.log(err);
+    res.status(400).send("Sorry, Fehler beim Abrufen des Aktienkurses.");
+  }
+});
+app.get("/", function(req, res) {
+  if (req.session.loogedin) {
+      dbClient.query("SELECT * FROM finance_overview WHERE user_id=$1 AND symbol=$2", [req.session.userid,'CASH'], function(dbError, dbResponse) {
+       let cash = Number(dbResponse.rows[0].total);
+       let total = cash;
+      dbClient.query("SELECT * FROM finance_overview WHERE user_id=$1 AND symbol!= $2", [req.session.userid, 'CASH'], function(dbError, dbResponse) {
+        if(dbResponse.rows.length != 0){
+          let overviewItems = dbResponse.rows;
+            for (let i = 0; i < overviewItems.length; i++) {
+              total += overviewItems[i].price * overviewItems[i].count;
+            }
+            total = Math.round(total*100)/100;
+            res.render("overview", {
+              overviewItems: overviewItems,
+              total: total,
+              cash: cash
+            });
+          } else {
+            res.render("overview", {
+              total: total,
+              cash: cash
+            })
+          }
+        });
+    });
+  } else res.render("landing");
+});
+app.get("/login", function(req, res) {
+  initSession(req.session);
   res.render("login");
 })
 
-app.post("/", urlencodedParser, function (req, res) {
-    var user = req.body.username;
-    var password = req.body.password;
-
-    dbClient.query("SELECT * FROM users WHERE username=$1 AND password=$2", [user, password], function (dbError, dbResponse) {
-        if (dbResponse.rows.length == 0) {
-            res.status(400).render("login", {
-                error: "Oops. Bitte überprüfen Sie Nutzername und Passwort!"
-            });
-        } else {
-            req.session.user = {
-                userId: dbResponse.rows[0].id
-            };
-          res.render("kursabfrage");
-        }
-    });
-});
-app.post("/login", urlencodedParser, function (req, res) {
-    var user = req.body.username;
-    var password = req.body.password;
-
-    dbClient.query("SELECT * FROM users WHERE username=$1 AND password=$2", [user, password], function (dbError, dbResponse) {
-        if (dbResponse.rows.length == 0) {
-            res.status(400).render("login", {
-                error: "Oops. Bitte überprüfen Sie Nutzername und Passwort!"
-            });
-        } else {
-            req.session.user = {
-                userId: dbResponse.rows[0].id
-            };
-          res.render("kursabfrage");
-        }
-    });
-});
-
-app.post("/register", urlencodedParser, function (req, res) {
+app.post("/login", urlencodedParser, function(req, res) {
   var user = req.body.username;
   var password = req.body.password;
-  var confirmation = req.body.confirmation;
 
-  dbClient.query("SELECT * FROM users WHERE username=$1", [user], function (dbError, dbResponse) {
-    if(dbResponse.rows.length != 0){
-        res.status(400).render("register", {
-        error: "Benutzername bereits vergeben"
-      });
-    }
-    else if(user == "" || password == ""){
+  initSession(req.session);
 
-      res.status(400).render("register",{
-        error:"Eingabe darf nicht leer sein"
+  dbClient.query("SELECT * FROM users WHERE username=$1", [user], function(dbError, dbResponse) {
+    if (dbResponse.rows.length == 0) {
+      res.status(400).render("login", {
+        error: "Oops. Bitte überprüfen Sie Nutzername und Passwort!"
       });
-    }
-    else if(password!=confirmation){
-      res.status(400).render("register", {
-        error: "Passwörter stimmen nicht überein"
+    } else {
+      let hash = dbResponse.rows[0].password;
+      bcrypt.compare(password, hash, function(err, result) {
+        if (result) {
+          req.session.userid = dbResponse.rows[0].user_id;
+          req.session.user = user;
+          req.session.loogedin = true;
+          console.log("login successful");
+          res.redirect("/");
+        } else {
+          res.status(400).render("login", {
+            error: "Oops. Bitte überprüfen Sie Nutzername und Passwort!"
+          });
+        }
       });
-    }
-
-    else{
-    dbClient.query("INSERT INTO users(username, password) VALUES ($1, $2)",[user, password],function(dbError, dbResponse){
-      res.render("login");
-    });
     }
   });
 });
 
-app.get("/kursabfrage", function (req, res) {
-    if (req.session.user != undefined) {
-          res.render("kursabfrage");
+app.post("/register", urlencodedParser, function(req, res) {
+  var user = req.body.username;
+  var password = req.body.password;
+  var confirmation = req.body.confirmation;
+
+  dbClient.query("SELECT * FROM users WHERE username=$1", [user], function(dbError, dbResponse) {
+    if (dbResponse.rows.length != 0) {
+      res.status(400).render("register", {
+        error: "Benutzername bereits vergeben"
+      });
+    } else if (user == "" || password == "") {
+      res.status(400).render("register", {
+        error: "Eingabe darf nicht leer sein"
+      });
+    } else if (password != confirmation) {
+      res.status(400).render("register", {
+        error: "Passwörter stimmen nicht überein"
+      });
     } else {
-        res.render("error", {
-            error: "You need to be logged in to access this page."
+      bcrypt.hash(password, saltRounds, function(err, hash) {
+        dbClient.query("INSERT INTO users(username, password) VALUES ($1, $2)", [user, hash], function(dbError, dbResponse) { //add user anad encrypted password in databank
+          dbClient.query("SELECT user_id FROM users WHERE username=$1", [user], function(dbError, dbResponse) { //get User id
+            let user_id = dbResponse.rows[0].user_id;
+            dbClient.query("INSERT INTO finance_overview(user_id, symbol, total) VALUES ($1, $2, $3)", [user_id, 'CASH', 10000.00]); //give
+            res.render("login");
+          });
         });
+      });
     }
+  });
 });
+app.get("/sell", function(req, res) {
+  if (req.session.loogedin) {
+    dbClient.query("SELECT * FROM finance_overview WHERE user_id=$1 AND count>0",[req.session.userid], function(dbError, dbResponse){
+      let tickerItems = dbResponse.rows;
+      res.render("sell",{
+        tickerItems: tickerItems
+      });
+    });
 
+  } else {
+    res.render("login", {
+      error: "You need to be logged in to access this page."
+    });
+  }
+});
+app.get("/history", function(req, res) {
+  if (req.session.loogedin) {
+    dbClient.query("SELECT * FROM finance_transactions WHERE user_id=$1", [req.session.userid], function(dbError, dbResponse) {
+      transactionItems = dbResponse.rows;
+      res.render("history", {
+        transactionItems: transactionItems
+      });
+    });
+  } else {
+    res.render("login", {
+      error: "You need to be logged in to access this page."
+    });
+  }
+});
+app.get("/account", function(req, res) {
+  if (req.session.loogedin) {
+    dbClient.query("SELECT * FROM finance_overview WHERE user_id=$1 AND symbol='CASH'", [req.session.userid], function(dbError, dbResponse) {
+      let total = dbResponse.rows[0].total;
+      res.render("account", {
+        username: req.session.user,
+        cash: total
+      });
+    });
+  } else {
+    res.render("login", {
+      error: "You need to be logged in to access this page."
+    });
+  }
+});
 app.get("/logout", function(req, res) {
-    req.session.destroy(function (err) {
-        console.log("Session destroyed.");
-    })
-    res.render("login");
+  req.session.destroy(function(err) {
+    console.log("Session destroyed.");
+  })
+  res.render("landing", {
+    logout: "You have been looged out"
+  });
 });
-
-app.get("/register", function(req, res){
+app.get("/register", function(req, res) {
   res.render("register");
 });
-
 
 app.listen(PORT, function() {
   console.log(`MI Finance running and listening on port ${PORT}`);
--- helpers.js
+++ helpers.js
@@ -6,9 +6,10 @@ dotenv.config();
 const API_KEY = process.env.API_KEY;
 
 async function lookup(tickerSymbol) {
-  let url = `https://cloud.iexapis.com/stable/stock/${tickerSymbol}/quote?token=${API_KEY}`;
+
+  let url =`https://cloud.iexapis.com/stable/stock/${tickerSymbol}/quote?token=${API_KEY}`;
   let response = await axios.get(url, { timeout: 5000 });
-  return response;
+  return response.data;
 }
 
 module.exports = {
